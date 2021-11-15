deb73f7ce95c9189c1766122102d7c436023af2d
Frucou <fruicou.sansus3@gmail.com>
2021-11-15 14:45:38 +0100

    Primer commit del ejercicio
    Añadido:
    -Primeras líneas del html con el enunciado del ejercicio 2
    -js con un alert de comprobación

---
+++ js/app.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+// Nodos
+const btn = document.querySelector(".btn")
+
+
+// Variables
+
+
+
+// Funciones
+const sacarCarta = () => {
+    alert();
+}
+
+
+// Eventos
+btn.addEventListener(
+    "click",
+    sacarCarta
+);
