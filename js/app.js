8cf3155f9f2eb07d52d1c2f50f9e3bb6ba8d7ac7
Frucou <fruicou.sansus3@gmail.com>
2021-11-19 09:04:55 +0100

    Resuelto el ejercicio 2
    El ejercicio 2 resuelto con math.floor y math.ranmdom

--- js/app.js
+++ js/app.js
@@ -3,16 +3,41 @@
 
 // Nodos
 const btn = document.querySelector(".btn")
+const resultado = document.querySelector(".resultado")
 
 
 // Variables
+const numRandom = () =>{ 
+    return Math.floor(Math.random() * (13 - 1) + 1);
+}
 
-
+const palos = () =>{ 
+    return Math.floor(Math.random() * (5 - 1) + 1);
+}
 
 // Funciones
 const sacarCarta = () => {
-    alert();
+    let numPalos = palos();
+    console.log(numPalos)
+    switch(numPalos) {
+        case 1: 
+        return resultado.innerHTML = "Número: "+ numRandom() + ` <p>Palos: Oros.</p>`;
+        
+        case 2: 
+        return resultado.innerHTML = "Número: "+ numRandom() + ` <p>Palos: Copas.</p>`;
+        
+        case 3: 
+        return resultado.innerHTML = "Número: "+ numRandom() + ` <p>Palos: Bastos.</p>`;
+        
+        case 4: 
+        return resultado.innerHTML = "Número: "+ numRandom() + ` <p>Palos: Espadas.</p>`;
+       
+
+    }
+   
+    
 }
+
 
 
 // Eventos
